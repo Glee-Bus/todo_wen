29318987f0615e4431d4d0bfa279bce130faf5db
Gleeb <>
2021-05-11 15:30:32 +0000

    добавили app config handlers прописали параметры бота в конфиге

---
+++ config.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = "1753958631:AAGsNc443JYgAgyXsemUYbwrnv5gtI2ueNM"
+admin_id = 787284728
+todo = {}
+HELP = """
+add  - добавить задачу
+show  - показать задачи
+done  - убрать задачу
+help  - помощь
+"""
